aef2d147465791ab429590dae28eabc7ae9e641c
lee0827ian <lee0827ian@gmail.com>
2026-04-06 16:23:18 +0900

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -4,19 +4,20 @@ window.APP_CONFIG = {
   GOOGLE_API_KEY: '',
   GOOGLE_CLIENT_ID: '',
   SPREADSHEET_ID: '',
-  KAKAO_REST_API_KEY: '',
-  KAKAO_JS_API_KEY: '',
+  KAKAO_REST_API_KEY: 'firebase deploy --only hosting',
+  KAKAO_JS_API_KEY: '47eed652b004605d8a8e3e39df268f24',
   ADMIN_PASSWORD: '1234',
 
   // Firebase (GCP project: github-475723)
   FIREBASE_SYNC_ENABLED: true,
   FIREBASE_ATTENDANCE_COLLECTION: 'schedule_attendance',
   FIREBASE_CONFIG: {
-    apiKey: '',
+    apiKey: 'AIzaSyBWF-w9FY608WIWypndo4SlUpvo2yjBqEo',
     authDomain: 'github-475723.firebaseapp.com',
     projectId: 'github-475723',
     storageBucket: 'github-475723.appspot.com',
-    messagingSenderId: '',
-    appId: '',
+    messagingSenderId: '564267897779',
+    appId: '1:564267897779:web:ed0d3a331bf6e1793b71c6',
+    measurementId: "G-THSTJ2KCX6",
   },
 };
